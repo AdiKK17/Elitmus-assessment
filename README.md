b68bb4f4541dd677eb7b83aa4d457f5466690688
AdiKK17 <kavikmr9@gmail.com>
2021-08-27 01:20:47 +0530

    basic models added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Elitmus-assessment
+
+1) Installation took some time on macbook m1. But over this hassle now.
+2) Not Using Scaffolding as it is recommended for beginners only and less to learn from it
--- elitmus/db/seeds.rb
+++ elitmus/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+advertisements = Advertisement.create([
+    {
+        title: "Audi",
+        description: "vorsprung durch technique"
+    },
+    {
+        title: "BMW",
+        description: "sheer driving pleasure"
+    },
+    {
+        title: "Mercedes",
+        description: "The Best or Nothing"
+    },
+])
+
+comments = Comment.create([
+    {
+        comment: "AUdi is great,ask tony stark",
+        advertisement: advertisements.first,
+    },
+    {
+        comment: "BMW is magnifique,ask Mission impossible people",
+        advertisement: advertisements.second,
+    },
+    {
+        comment: "Mercedes is class,ask diplomats",
+        advertisement: advertisements.third,
+    },
+])
